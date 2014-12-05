ae718486bd7bd9f579e74997fff0cba6372cce8b
Jake <jisawyer@iastate.edu>
2014-12-05 15:56:03 -0600

    partial web app

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+import crime_stats as cs
+import pandas as pd
+import random
+import cStringIO
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/welcome')
+def welcome():
+	return render_template('welcome.html')
+
+@app.route('/coolmap')
+def coolmap():
+	return render_template('chiCrimesCircles.html')
+
+@app.route('/fact')
+def fact():
+	df = cs.get_df()
+	maxcrime = cs.most_dangerous_block(df)
+	facts = [cs.most_dangerous_block(df), cs.worst_place(df), cs.domestic(df), cs.community(df), cs.theft(df)]
+	fact_num = random.randint(0,4)
+	if fact_num == 0 or fact_num == 2: 
+		return render_template('fact.html', fact=facts[fact_num])
+	if fact_num == 1:
+		return render_template('fact.html', fact=facts[fact_num], graph=1)
+
+@app.route('/mypng')
+def mypng(graph):
+	graph = worst_place_graph(df)
+	f = cStringIO.cStringIO()
+	graph.savefig(f, format='png')
+	data = f.seek(0)
+	return send_file(data, mimetype='image/png')
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+	
+
+
+
+
+	
--- crime_stats.py
+++ crime_stats.py
@@ -9,12 +9,14 @@ import time
 import webbrowser
 import os
 
-def main():
+def get_df():
     url = 'http://data.cityofchicago.org/resource/ijzp-q8t2.json'
     r = requests.get(url)
     data = r.text
-
     df = pd.read_json(data)
+    return df
+
+def arrests(df):
     t = 0
     f = 0
     for i in df['arrest']:
@@ -22,6 +24,73 @@ def main():
             t += 1
         if not i:
             f +=1
+    return "Out of 1000 crimes that the Chicago Police responded to this year %s ended in arrests while %s didn't.\n" % (t, f)
+
+def most_dangerous_block(df):
+    crime_per_community = Counter(df['block'])
+    number = max(crime_per_community.values())
+    maxcrime = max(crime_per_community)
+    result =  "The block with the most crimes on it is %s, with a total of %s crimes from our 1000 random crime sample.\n" % (str(maxcrime).lower(), number)
+    return result
+
+def most_dangerous_block_url(maxcrime):
+    formatted_maxcrime = str(maxcrime).lower().replace('x', '0') + 'chicago il'
+    return 'http://maps.google.com/?q=%s' % formatted_maxcrime
+    
+def worst_place(df):
+    max_loc = df['location_description']
+    locals = []
+    for i in df['location_description']:
+        locals.append(i)
+    locals = Counter(locals)
+    loc = max(max_loc)
+    result =  "A crime is most likely to happen in a %s environment.\n" % str(loc).lower()
+    return result
+
+def worst_place_graph(df):
+    max_loc = df['location_description']
+    locals = []
+    for i in df['location_description']:
+        locals.append(i)
+    locals = Counter(locals)
+    return make_dict_bar_graph(locals)
+
+def domestic(df):
+    domestic_count = 0
+    for i in df['domestic']:
+        if i == True:
+            domestic_count += 1
+    domestic_crime_percent = (float(domestic_count)/1000.0)*100.0
+    result =  "In the city of Chicago, from what our random of 1000 sample crimes says, " + str(domestic_crime_percent) +"%  of crimes are domestic." + "That is %s domestic cases each year out of our sample size.\n" % domestic_count
+    return result
+
+def theft(df):
+    return "Here you can see that the most occuring crime in Chicago is theft, so keep your valuables in a safe place!\n"
+
+def theft_graph(df):
+    distCount = Counter(df['district'])
+    typeCount = Counter(df['primary_type'])
+    typeCommonKey = typeCount.most_common()
+
+    return make_dict_bar_graph(typeCount)
+
+def community(df):
+    communityCrime = Counter(df['community_area'])
+    
+    most_common_dist = communityCrime.most_common()
+    most_common_dist = most_common_dist[0]
+
+    return "Moving on to the next graph, you can see that community %s is involved with the most crime by almost double the amount of the next highest, with %s crimes.\n" % (most_common_dist[0], most_common_dist[1])
+
+def community_graph():
+    make_dict_bar_graph(communityCrime)
+
+def location_graph():
+    locationCount = Counter(df['location_description'])
+    
+    return make_dict_bar_graph(locationCount)
+
+def main():
     print "Welcome to our MIS 407 project!"
     time.sleep(7)
     print("This is a little story about crime in the city of Chicago IL")
@@ -33,56 +102,18 @@ def main():
     print "This set of data is different every time you run this script"
     time.sleep(7)
     print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-    print "Out of 1000 crimes that the Chicago Police responded to this year %s ended in arrests while %s didn't.\n" % (t, f)
     time.sleep(7)
-    crime_per_community = Counter(df['block'])
-    number = max(crime_per_community.values())
-    maxcrime = max(crime_per_community)
-    print "The block with the most crimes on it is %s, with a total of %s crimes from our 1000 random crime sample.\n" % (str(maxcrime).lower(), number)
-    time.sleep(7)
-    print "Here's a map of the block!"
-    time.sleep(7)
-    formatted_maxcrime = str(maxcrime).lower().replace('x', '0') + 'chicago il'
-    webbrowser.open('http://maps.google.com/?q=%s' % formatted_maxcrime)
-    time.sleep(7)
-    max_loc = df['location_description']
-    locals = []
-    for i in df['location_description']:
-        locals.append(i)
-    locals = Counter(locals)
-    loc = max(max_loc)
-    print "A crime is most likely to happen in a %s environment.\n" % str(loc).lower()
-    time.sleep(7)
-    print "Take a look at the attached graph if you want some more info on where crime are most likely to take place in Chicago!\n"
-    time.sleep(7)
-    make_dict_bar_graph(locals)
-
-    domestic_count = 0
-    for i in df['domestic']:
-        if i == True:
-            domestic_count += 1
-    domestic_crime_percent = (float(domestic_count)/1000.0)*100.0
-    print "In the city of Chicago, from what our random of 1000 sample crimes says, " + str(domestic_crime_percent) +"%  of crimes are domestic.\n"
-    time.sleep(7)
-    print "That is %s domestic cases each year out of our sample size.\n" % domestic_count
-    time.sleep(7)
-
-    distCount = Counter(df['district'])
-    typeCount = Counter(df['primary_type'])
-    typeCommonKey = typeCount.most_common()
-
-    make_dict_bar_graph(typeCount)
-
-    print "Here you can see that the most occuring crime in Chicago is theft, so keep your valuables in a safe place!\n"
+  
     time.sleep(7)
     
-    communityCrime = Counter(df['community_area'])
-    make_dict_bar_graph(communityCrime)
-    
-    most_common_dist = communityCrime.most_common()
-    most_common_dist = most_common_dist[0]
+    time.sleep(7)
 
-    print "Moving on to the next graph, you can see that community %s is involved with the most crime by almost double the amount of the next highest, with %s crimes.\n" % (most_common_dist[0], most_common_dist[1])
+    
+
+
+    time.sleep(7)
+    
+    
     time.sleep(7)
     print "It's also interesting to note that the neighboring communities also have a consistent crime rate."
     time.sleep(7)
@@ -103,7 +134,7 @@ def make_dict_bar_graph(data):
     plt.bar(range(len(data)), data.values())
     plt.xticks(range(len(data)), data.keys(), rotation=90)
     matplotlib.rc('xtick', labelsize=9)
-    plt.show()
+    return plt
 
 if __name__ == "__main__":
     main()
